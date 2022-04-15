c0382307c3f051d49b2f5b551bcc1eed94f8310a
figotj <leitao8708@gmail.com>
2022-04-15 13:06:32 -0400

    Update demo.py

--- Data/demo.py
+++ Data/demo.py
@@ -1,3 +1,10 @@
+#The 8 million data can be found in zenodo
+#https://sandbox.zenodo.org/record/949154#.YXrF5hrMKUm
+#DOI:10.5072/zenodo.949154
+
 import pickle 
 
 df_8million = pickle.load(open("Pred_SA_8million.pickle","rb"))
+
+
+
